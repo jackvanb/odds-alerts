4e73582d6c777730a096b99efbf518dae1b58e0b
Jack Van Boening <jackvanboening@gmail.com>
2021-01-22 15:04:38 -0800

    Adjust value odd threshold

--- constants.js
+++ constants.js
@@ -17,7 +17,7 @@ exports.smsFrom = process.env.SMS_FROM;
 exports.smsTo = ['+19162206791', '+19549808358'];
 
 // Constants
-exports.ODDS_ADJUSTMENT = 0.1;
+exports.ODDS_ADJUSTMENT = 0.15;
 exports.OVERALL_STAKE = 100;
 exports.SPORT_KEYS = [
   'aussierules_afl',
